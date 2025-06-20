c599f0e2dbf3c94ee5b468c8c99894b412a9189f
Kyle Ramachandran <kyleramachandran@berkeley.edu>
2025-06-20 12:30:59 +0800

    lint and prettier

--- src/api/useMqtt.ts
+++ src/api/useMqtt.ts
@@ -14,12 +14,7 @@ export const useMqtt = () => {
     if (clientRef.current) return;
 
     try {
-      clientRef.current = new Paho.Client(
-        "dev.gaiv.my",
-        9001,
-        "/ws",
-        clientId,
-      );
+      clientRef.current = new Paho.Client("dev.gaiv.my", 9001, "/ws", clientId);
       setIsClientReady(true);
     } catch (error) {
       console.error("Error creating MQTT client:", error);

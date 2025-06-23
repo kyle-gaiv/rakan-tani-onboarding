3bccf0cab65d7d2be3904d6a950dccd56a01bd0c
Kyle Ramachandran <kyleramachandran@berkeley.edu>
2025-06-23 14:35:43 +0800

    added onboarding confirmation page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -51,6 +51,10 @@ export default function WelcomePage() {
   const [name, setName] = useState<string>("");
   const [location, setLocation] = useState<LatLngLiteral | undefined>();
   const [selectedDay, setSelectedDay] = useState<string | null>(null);
+  const [completedOnboarding, setCompletedOnboarding] =
+    useState<boolean>(false);
+  //eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [rerenderFlag, setRerenderFlag] = useState<number>(0);
   const { publish } = useMqttContext();
   const searchParams = useSearchParams();
   const { id: sessionID } = useID();
@@ -94,6 +98,10 @@ export default function WelcomePage() {
     try {
       publish("onboard", message); // send info to MQTT topic
       toast.success("Information saved successfully!");
+      setTimeout(() => {
+        setCompletedOnboarding(true);
+      }, 500); // Delay to allow toast to show
+      setRerenderFlag((prev) => prev + 1); // Trigger rerender
     } catch (error) {
       console.error("Error publishing to MQTT:", error);
       toast.error(
@@ -112,8 +120,8 @@ export default function WelcomePage() {
       setMermaidLoading(false);
     };
 
-    generateSchedule();
-  }, []);
+    if (!completedOnboarding) generateSchedule();
+  }, [completedOnboarding]);
 
   // Handler for click events in mermaid diagram
   // Sets nodeId to the ID of the clicked node (hlt) to trigger HLT query
@@ -138,79 +146,116 @@ export default function WelcomePage() {
 
   return (
     <div className="flex flex-col items-start justify-start min-h-screen bg-gray-100 p-8 gap-8 text-black">
-      <div className="flex justify-center w-full">
-        <h1 className="text-4xl font-bold mb-4">Onboarding Page</h1>
-      </div>
-      <div className="w-full">
-        <Section>
-          <div className="flex flex-col items-start justify-start gap-4 w-full">
-            <h2 className="text-2xl font-semibold">Enter Your Information</h2>
-            <div className="grid grid-cols-3 gap-8 w-full">
+      {completedOnboarding && (
+        <div className="flex flex-col items-center justify-center w-full gap-4">
+          <h1 className="text-4xl font-bold">
+            Thank you for completing the Rakan Tani onboarding process!
+          </h1>
+          <p className="text-xl font-semibold">
+            Return to WhatsApp to continue
+          </p>
+          <p className="mt-8 text-xl font-semibold">
+            If you would like to edit your information, press the button below
+          </p>
+          <Button
+            label="Return to Information Page"
+            onClick={() => {
+              setCompletedOnboarding(false);
+              setTimeout(() => {
+                setRerenderFlag((prev) => prev + 1); // Trigger rerender
+                setMermaidChartText(mermaidChartText);
+              }, 500); // Delay to allow state change to take effect
+            }}
+          />
+        </div>
+      )}
+      {!completedOnboarding && (
+        <>
+          <div className="flex justify-center w-full">
+            <h1 className="text-4xl font-bold mb-4">Onboarding Page</h1>
+          </div>
+          <div className="w-full">
+            <Section>
               <div className="flex flex-col items-start justify-start gap-4 w-full">
-                <TextInput
-                  label="Name"
-                  placeholder="Enter your name"
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                  id="name"
-                  error={!!formErrors.name}
-                  errorText={formErrors.name}
-                />
-                <DropdownInput
-                  label="Rice Type"
-                  id="rice-type"
-                  value={selectedRiceType}
-                  onChange={(e) => setSelectedRiceType(Number(e.target.value))}
-                  options={riceTypeOptions.map((type, index) => ({
-                    value: index.toString(),
-                    label: type,
-                  }))}
-                  error={!!formErrors.ricetype}
-                  errorText={formErrors.ricetype}
+                <h2 className="text-2xl font-semibold">
+                  Enter Your Information
+                </h2>
+                <div className="grid grid-cols-3 gap-8 w-full">
+                  <div className="flex flex-col items-start justify-start gap-4 w-full">
+                    <TextInput
+                      label="Name"
+                      placeholder="Enter your name"
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                      id="name"
+                      error={!!formErrors.name}
+                      errorText={formErrors.name}
+                    />
+                    <DropdownInput
+                      label="Rice Type"
+                      id="rice-type"
+                      value={selectedRiceType}
+                      onChange={(e) =>
+                        setSelectedRiceType(Number(e.target.value))
+                      }
+                      options={riceTypeOptions.map((type, index) => ({
+                        value: index.toString(),
+                        label: type,
+                      }))}
+                      error={!!formErrors.ricetype}
+                      errorText={formErrors.ricetype}
+                    />
+                  </div>
+                  <div className="col-span-2 w-full h-full">
+                    {/* Can set initial position by taking from database if present */}
+                    <LocationInputDynamic
+                      setLocation={setLocation}
+                      initialPosition={location}
+                      error={!!formErrors.location}
+                      errorText={formErrors.location}
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-col items-start justify-start gap-4 w-full mt-2">
+                <h3 className="text-lg font-bold">
+                  Click a day to select your current stage
+                </h3>
+                {!!formErrors.day && (
+                  <p className="text-sm text-red-500">{formErrors.day}</p>
+                )}
+                <div
+                  className={`w-full overflow-x-auto bg-white rounded-md p-4 ${!!formErrors.day ? "border border-red-500" : ""}`}
+                >
+                  {mermaidLoading ? (
+                    <div className="text-lg text-black text-center font-semibold">
+                      Loading...
+                    </div>
+                  ) : (
+                    <div
+                      className="shrink-0 h-auto"
+                      style={{ minWidth: `9000px` }}
+                    >
+                      <MermaidDiagramDynamic
+                        className="h-auto"
+                        securityLevel="loose"
+                      >
+                        {mermaidChartText}
+                      </MermaidDiagramDynamic>
+                    </div>
+                  )}
+                </div>
+              </div>
+              <div className="flex flex-row items-center justify-end w-full mt-2">
+                <Button
+                  label="Save Information"
+                  onClick={handleSaveInformation}
                 />
               </div>
-              <div className="col-span-2 w-full h-full">
-                {/* Can set initial position by taking from database if present */}
-                <LocationInputDynamic
-                  setLocation={setLocation}
-                  initialPosition={undefined}
-                  error={!!formErrors.location}
-                  errorText={formErrors.location}
-                />
-              </div>
-            </div>
+            </Section>
           </div>
-          <div className="flex flex-col items-start justify-start gap-4 w-full mt-2">
-            <h3 className="text-lg font-bold">
-              Click a day to select your current stage
-            </h3>
-            {!!formErrors.day && (
-              <p className="text-sm text-red-500">{formErrors.day}</p>
-            )}
-            <div
-              className={`w-full overflow-x-auto bg-white rounded-md p-4 ${!!formErrors.day ? "border border-red-500" : ""}`}
-            >
-              {mermaidLoading ? (
-                <div className="text-lg text-black text-center font-semibold">
-                  Loading...
-                </div>
-              ) : (
-                <div className="shrink-0 h-auto" style={{ minWidth: `9000px` }}>
-                  <MermaidDiagramDynamic
-                    className="h-auto"
-                    securityLevel="loose"
-                  >
-                    {mermaidChartText}
-                  </MermaidDiagramDynamic>
-                </div>
-              )}
-            </div>
-          </div>
-          <div className="flex flex-row items-center justify-end w-full mt-2">
-            <Button label="Save Information" onClick={handleSaveInformation} />
-          </div>
-        </Section>
-      </div>
+        </>
+      )}
     </div>
   );
 }
